e5e99df37554c585b0044fba0d6bb09bb1971f9b
RUDepure <alexrum92@gmail.com>
2021-10-07 10:04:04 -0500

    initial commit

---
+++ Overloading_Exercise/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Overloading_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer1 = Add(2, 2);
+            var answer2 = Add(2466m, .2145m);
+            var answer3 = Add(10, 20, true);
+
+            Console.WriteLine(answer1);
+            Console.WriteLine(answer2);
+            Console.WriteLine(answer3);
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool c)
+        {
+            string message = "";
+            int result;
+            result = a + b;
+
+            if (c == true)
+            {
+                
+
+                if (result == 1)
+                    message = $"{result} dollar";
+                else
+                    message = $"{result} dollars";
+
+                return message;
+            }
+            else
+                return $"{result}";
+
+        }
+    }
+}
